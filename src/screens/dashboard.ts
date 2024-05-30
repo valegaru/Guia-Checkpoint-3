177fb583bc82e78a3c9192fd6ef78d5e426aa550
valegaru <valegaru2003@gmail.com>
2024-05-29 20:10:24 -0500

    screens

---
+++ src/screens/dashboard.ts
@@ -0,0 +1,22 @@
+import { addObserver, dispatch } from '../store/index';
+import { navigate } from '../store/actions';
+class Dashboard extends HTMLElement {
+	constructor() {
+		super();
+		this.attachShadow({ mode: 'open' });
+	}
+
+	connectedCallback() {
+		this.render();
+		const button = this.shadowRoot?.querySelector('button');
+		button?.addEventListener('click', () => {
+			dispatch(navigate('LOGIN'));
+		});
+	}
+
+	render() {
+		if (this.shadowRoot) this.shadowRoot.innerHTML = '<h1>Dashboard</h1><button>Click</button>';
+	}
+}
+
+customElements.define('app-dashboard', Dashboard);
---
+++ src/screens/login.ts
@@ -0,0 +1,26 @@
+import { addObserver, dispatch } from '../store/index';
+import { navigate } from '../store/actions';
+
+class Login extends HTMLElement {
+	constructor() {
+		super();
+		this.attachShadow({ mode: 'open' });
+	}
+
+	connectedCallback() {
+		this.render();
+		const button = this.shadowRoot?.querySelector('button');
+		button?.addEventListener('click', () => {
+			dispatch(navigate('DASHBOARD'));
+		});
+	}
+
+	render() {
+		if (this.shadowRoot) {
+			this.shadowRoot.innerHTML = `<h1>Login</h1>
+			<button>Click</button>`;
+		}
+	}
+}
+
+customElements.define('app-login', Login);
