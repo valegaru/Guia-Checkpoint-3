001b8c7d19be3bf0443392a98fc1b6ab18553350
valegaru <valegaru2003@gmail.com>
2024-05-29 22:33:41 -0500

    observer render from firebase

--- src/screens/dashboard.ts
+++ src/screens/dashboard.ts
@@ -3,15 +3,16 @@ import { navigate } from '../store/actions';
 import { Product } from '../types/products';
 import firebase from '../services/firebase';
 
-const formData: Omit<Product, "id"> = {
-  name: "",
-  price: 0,
+const formData: Omit<Product, 'id'> = {
+	name: '',
+	price: 0,
 };
 
 export class Dashboard extends HTMLElement {
 	constructor() {
 		super();
 		this.attachShadow({ mode: 'open' });
+		addObserver(this); //necesito este observer aqui para que me vuleva a renderizar segun la informacion del firebase
 	}
 
 	connectedCallback() {
@@ -23,54 +24,54 @@ export class Dashboard extends HTMLElement {
 	}
 
 	submitForm() {
-    console.log(formData);
-    firebase.addProduct(formData);
-  }
+		console.log(formData);
+		firebase.addProduct(formData);
+	}
 
-  changeName(e: any) {
-    formData.name = e?.target?.value;
-  }
+	changeName(e: any) {
+		formData.name = e?.target?.value;
+	}
 
-  changePrice(e: any) {
-    formData.price = Number(e?.target?.value);
-  }
+	changePrice(e: any) {
+		formData.price = Number(e?.target?.value);
+	}
 
 	async render() {
 		if (this.shadowRoot) this.shadowRoot.innerHTML = '<h1>Dashboard</h1><button>Click</button>';
 
-		const title = this.ownerDocument.createElement("h1");
-    title.innerText = "Añade producto";
-    this.shadowRoot?.appendChild(title);
+		const title = this.ownerDocument.createElement('h1');
+		title.innerText = 'Añade producto';
+		this.shadowRoot?.appendChild(title);
 
-    const pName = this.ownerDocument.createElement("input");
-    pName.placeholder = "nombre del producto";
-    pName.addEventListener("change", this.changeName);
-    this.shadowRoot?.appendChild(pName);
+		const pName = this.ownerDocument.createElement('input');
+		pName.placeholder = 'nombre del producto';
+		pName.addEventListener('change', this.changeName);
+		this.shadowRoot?.appendChild(pName);
 
-    const pPrice = this.ownerDocument.createElement("input");
-    pPrice.placeholder = "price";
-    // pPrice.type = "number";
-    pPrice.addEventListener("change", this.changePrice);
-    this.shadowRoot?.appendChild(pPrice);
+		const pPrice = this.ownerDocument.createElement('input');
+		pPrice.placeholder = 'price';
+		// pPrice.type = "number";
+		pPrice.addEventListener('change', this.changePrice);
+		this.shadowRoot?.appendChild(pPrice);
 
-    const save = this.ownerDocument.createElement("button");
-    save.innerText = "Guardar";
-    save.addEventListener("click", this.submitForm);
-    this.shadowRoot?.appendChild(save);
+		const save = this.ownerDocument.createElement('button');
+		save.innerText = 'Guardar';
+		save.addEventListener('click', this.submitForm);
+		this.shadowRoot?.appendChild(save);
 
-    const products = await firebase.getProducts();
-    products.forEach((p: Product) => {
-      const container = this.ownerDocument.createElement("section");
-      const name = this.ownerDocument.createElement("h3");
-      name.innerText = p.name;
-      container.appendChild(name);
+		const products = await firebase.getProducts();
+		products.forEach((p: Product) => {
+			const container = this.ownerDocument.createElement('section');
+			const name = this.ownerDocument.createElement('h3');
+			name.innerText = p.name;
+			container.appendChild(name);
 
-      const price = this.ownerDocument.createElement("h3");
-      price.innerText = String(p.price);
-      container.appendChild(price);
+			const price = this.ownerDocument.createElement('h3');
+			price.innerText = String(p.price);
+			container.appendChild(price);
 
-      this.shadowRoot?.appendChild(container);
-    });
+			this.shadowRoot?.appendChild(container);
+		});
 	}
 }
 
