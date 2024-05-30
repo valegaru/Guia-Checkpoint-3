eac77571e9cecac745e07ee05fec0658f41c8106
valegaru <valegaru2003@gmail.com>
2024-05-29 19:57:19 -0500

    importar observer y appState en index abuelo

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import "./components/export"
+import { addObserver,appState } from "./store"; //importar observers y appState
 
 class AppContainer extends HTMLElement {
     constructor(){
         super();
         this.attachShadow({mode: "open"})
+        addObserver(this)//le estoy diciendo que appContainer va a tener un observador el cual avisa cuando el appState cambia
     }
 
     connectedCallback() {
