fd6920fb559a1d3fd2715580f19418374861fa3c
valegaru <valegaru2003@gmail.com>
2024-05-29 22:08:45 -0500

    get and add to firebase

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -2,6 +2,7 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore'; //Importar los modulos
 import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth';
 import { collection, addDoc, getDocs, doc, setDoc } from 'firebase/firestore'; //Importar funciones para agregar info a la db
+import { Product } from '../types/products';
 
 const firebaseConfig = {
 	apiKey: 'AIzaSyBYbGMD0aW7X5vbeJ2jSI9oZfm57ltn1QI',
@@ -17,3 +18,35 @@ const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
 const auth = getAuth(app);
+
+
+export const addProduct = async (product: Omit<Product, "id">) => {
+  try {
+    //agrega un documento en la collection products
+    const where = collection(db, "products");
+    await addDoc(where, product);
+    console.log("se añadió con éxito");
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const getProducts = async () => {
+  //saca los documentos de la collection products y les toma una foto guardandolos en la constante
+  const querySnapshot = await getDocs(collection(db, "products"));
+  const transformed: Array<Product> = [];
+
+  //recorre lo que se guardo
+  querySnapshot.forEach((doc) => {
+    //no trae el id
+    const data: Omit<Product, "id"> = doc.data() as any;
+    transformed.push({ id: doc.id, ...data });
+  });
+
+  return transformed;
+};
+
+export default {
+  addProduct,
+  getProducts,
+};
